ad3dba2913651449a53d0025e0b82759f847596b
Amir Soltane <khalid.soltane@gmail.com>
2023-12-30 13:51:06 +0100

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,11 @@
 package ma.enset;
-
+/*
+ * ************
+ */
 public class Test{
+    /*
+     * ************
+     */
     public static void main(String[] args){
         System.out.println("Hello!");
     }
